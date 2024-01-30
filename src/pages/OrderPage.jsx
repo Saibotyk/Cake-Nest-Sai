fe1f0b14933b7ca5cda15d7378d7d9c1336815d1
Saibotyk <tobias13@hotmail.fr>
2024-01-30 14:25:52 +0100

    ✅  Add orderPage and NavBar - utils create for a reload function

--- src/pages/OrderPage.jsx
+++ src/pages/OrderPage.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import { Link } from 'react-router-dom';
+
+import { theme } from '../components/Theme';
+import styled from 'styled-components'
+import NavBar from '../components/reusable_ui/NavBar';
 
 export default function OrderPage() {
 
-  const {username} = useParams();
+  const { username } = useParams();
 
   return (
-    <div>
-      Bonjour {username}
-      <Link to="/">
-        <button>Se deconnecter</button>
-      </Link>
-    </div>
+    <BackgroundStyled>
+      <NavBar username={username}/>
+    </BackgroundStyled>
   )
 }
+
+const BackgroundStyled = styled.div`
+  height: 100vh;
+  ::before{
+    content: "";
+    position: absolute  ;
+    top: 0;
+    left: 0;
+    z-index: -1000;
+    background-color: ${theme.colors.primary};
+    height: 100vh;
+    width: 100vw;
+  }
+`;
