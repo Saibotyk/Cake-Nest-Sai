e474c40a726510084de883be8d12cd55c07e7931
Saibotyk <tobias13@hotmail.fr>
2024-01-30 11:16:52 +0100

    first bug fix

--- src/components/ConnectForm.jsx
+++ src/components/ConnectForm.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import styled from 'styled-components'
+import { theme } from './Theme/index'
+import { BsChevronRight } from "react-icons/bs";
+
 
 export default function ConnectForm() {
     const [name, setName] = useState("")
     const navigate = useNavigate();
-    
+
     const handleSubmit = (event) => {
         if (name === "") {
             alert("Ce champ ne peut être vide")
@@ -21,13 +25,65 @@ export default function ConnectForm() {
         setName(event.target.value)
     }
 
+
+
     return (
         <div>
-            <h2>Connectez-vous</h2>
-            <form action="submit" onSubmit={handleSubmit}>
-                <input type="text" placeholder='Entrez votre prénom...' onChange={handleChangeName} value={name}/>
-                <button>Accédez à votre espace</button>
-            </form>
+            <StyledH2 className='title'>Bienvenue chez nous !</StyledH2>
+            <StyledHr />
+            <StyledH3>Connectez-vous</StyledH3>
+            <StyledForm action="submit" onSubmit={handleSubmit}>
+                <StyledInput type="text" placeholder='Entrez votre prénom...' onChange={handleChangeName} value={name} />
+                <StyledButton>
+                    Mon espace <BsChevronRight />
+                </StyledButton>
+            </StyledForm>
         </div>
     )
 }
+
+const StyledButton = styled.button`
+    background-color: ${theme.colors.primary};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: white;
+    height: ${theme.spacing.xxl};
+    font-family: 'Open Sans', sans-serif;
+    font-weight: ${theme.fonts.weights.bold};
+    font-size: ${theme.fonts.size.P1};
+    border: none;
+    border-radius: ${theme.borderRadius.round};
+`;
+
+const StyledHr = styled.hr`
+    height: ${theme.spacing.xxs};
+    background-color: ${theme.colors.primary};
+    border: none;
+`;
+
+const StyledForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: ${theme.spacing.lg};
+`;
+
+const StyledInput = styled.input`
+    height: ${theme.spacing.xxl};
+    border-radius: ${theme.borderRadius.round};
+    border: none;
+`;
+
+const StyledH2 = styled.h2`
+    font-family: 'Pacifico', cursive;
+    text-align: center;
+    font-size: ${theme.fonts.size.P6};
+    color: ${theme.colors.white};
+`;
+
+const StyledH3 = styled.h3`
+    font-family: 'Pacifico', cursive;
+    text-align: center;
+    font-size: ${theme.fonts.size.P4};
+    color: ${theme.colors.white};
+`;
--- src/pages/ConnectPage.jsx
+++ src/pages/ConnectPage.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import ConnectForm from '../components/ConnectForm'
+import styled from 'styled-components'
+import bg from '../assets/tarts.jpg'
+import Logo from '../components/reusable_ui/Logo'
 
 export default function ConnectPage() {
+
+
   return (
-    <div>
-      <h1>Bienvenue chez nous !</h1>
+    <StyledDiv>
+      <Logo />
       <ConnectForm />
-    </div>
+    </StyledDiv>
   )
 }
+
+  const StyledDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin:0 auto;
+    height: 100vh;
+
+    ::before{
+      background-color: black;
+      /* background-image: linear-gradient(to right, rgb(0 0 0 / 100%), rgb(0 0 0 / 0%)), url(${bg}); */
+    }
+  `;
+
